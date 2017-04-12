79ba11dc68bdd81962a605621003ef9f0dc85054
Gseven <gse7en2010@163.com>
2017-04-12 18:40:11 +0800

    提交一下

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,39 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
+
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { DataQueryComponent } from './data-query/data-query.component';
+import { SettingComponent } from './setting/setting.component';
+
+
+
+const appRoutes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'query', component: DataQueryComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'setting', component: SettingComponent },
+  // { path: 'test', component: TestComponent },
+  // { path: 'socket', component: SocketComponent, canActivate: [AuthGuard] }
+]
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DataQueryComponent,
+    SettingComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/data-query/data-query.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-query',
+  templateUrl: './data-query.component.html',
+  styleUrls: ['./data-query.component.css']
+})
+export class DataQueryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
